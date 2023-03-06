6e1b245e5d137ea91ee5a697dcfbea7cbaf1015b
alan.wu <snfzi@qq.com>
2023-03-06 19:33:26 +0800

    feat:first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+module.exports={
+    "extends": ["@antfu"],
+  "overrides": [
+    {
+      "files": ["*.vue"],
+      "rules": {
+        "vue/component-tags-order": [
+          "error",
+          {
+            "order": [["template", "script"], "style"]
+          }
+        ]
+      }
+    },
+    {
+      "files": ["*.ts", "*.js", "*.vue"],
+      "rules": {
+        "no-console": "off",
+        "no-alert": "off",
+        "curly": ["error", "all"]
+      }
+    }
+  ]
+}
